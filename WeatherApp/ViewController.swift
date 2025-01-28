84595b185ce746a1b73f144c66f31c3edd742eda
Shamanth R <shamanth.r@creditone.com>
2025-01-28 14:40:48 +0530

    project setup basic

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Shamanth R on 28/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
